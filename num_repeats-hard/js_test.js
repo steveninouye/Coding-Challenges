d040c3c28b044f18cb1e84fd48bc1398655b8274
Steven Inouye <steveninouye@msn.com>
2018-08-06 11:29:17 -1000

    solved num repeats in js

---
+++ num_repeats-hard/js_test.js
@@ -0,0 +1,45 @@
+// Write a method that takes in a string and returns the number of
+// letters that appear more than once in the string. You may assume
+// the string contains only lowercase letters. Count the number of
+// letters that repeat, not the number of times they repeat in the
+// string.
+//
+// Difficulty: hard.
+
+function num_repeats(string) {
+    let foundLetters = [];
+    let strArr = string.split('');
+    strArr.forEach((letter, index) => {
+        let testArr = strArr.slice();
+        testArr.splice(index, 1);
+        if (
+            testArr.indexOf(letter) !== -1 &&
+            foundLetters.indexOf(letter) === -1
+        ) {
+            foundLetters.push(letter);
+        }
+    });
+    return foundLetters.length;
+}
+
+// These are tests to check that your code is working. After writing
+// your solution, they should all print true.
+console.log('\nTests for #num_repeats');
+console.log('===============================================');
+console.log(
+    'num_repeats("abdbc") === 1: ' + (num_repeats('abdbc') === 1).toString()
+);
+// one character is repeated
+console.log(
+    'num_repeats("aaa") === 1: ' + (num_repeats('aaa') === 1).toString()
+);
+console.log(
+    'num_repeats("abab") === 2: ' + (num_repeats('abab') === 2).toString()
+);
+console.log(
+    'num_repeats("cadac") === 2: ' + (num_repeats('cadac') === 2).toString()
+);
+console.log(
+    'num_repeats("abcde") === 0: ' + (num_repeats('abcde') === 0).toString()
+);
+console.log('===============================================');
