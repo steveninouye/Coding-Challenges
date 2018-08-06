c04dfc6560bb374be786c2d11ab5503733e5253a
Steven Inouye <steveninouye@msn.com>
2018-08-06 10:10:50 -1000

    completed greatest common factor in ruby

---
+++ greatest_common_factor-med/rb_test.rb
@@ -0,0 +1,37 @@
+# Write a method that takes in two numbers. Return the greatest
+# integer that evenly divides both numbers. You may wish to use the
+# `%` modulo operation.
+#
+# Difficulty: medium.
+
+def greatest_common_factor(number1, number2)
+  divider = 1
+  large_num = number1 >= number2 ? number1 : number2
+  small_num = large_num == number1 ? number2 : number1
+  while divider <= small_num
+    result = small_num / divider
+    if large_num % result == 0
+      return result
+    end
+    divider += 1
+  end
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts("\nTests for #greatest_commmon_factor")
+puts("===============================================")
+    puts(
+      'greatest_common_factor(3, 9) == 3: ' +
+      (greatest_common_factor(3, 9) == 3).to_s
+    )
+    puts(
+      'greatest_common_factor(16, 24) == 8: ' +
+      (greatest_common_factor(16, 24) == 8).to_s
+    )
+    puts(
+      'greatest_common_factor(3, 5) == 1: ' +
+      (greatest_common_factor(3, 5) == 1).to_s
+    )
+puts("===============================================")
